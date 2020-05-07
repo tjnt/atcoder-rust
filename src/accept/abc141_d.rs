fdb698ab3490fb675d5e8aa83d953ca1da0db79c
tjnt <3m2dyzxu@gmail.com>
2020-05-08 03:02:39 +0900

    add ac

---
+++ src/accept/abc141_d.rs
@@ -0,0 +1,65 @@
+/* {{{ */
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+/* }}} */
+
+use std::collections::binary_heap::BinaryHeap;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        a: [u64;n]
+    }
+    let mut h = BinaryHeap::<u64>::new();
+    h.extend(a.iter());
+    let mut c = 0;
+    while c < m {
+        let mut x = h.peek_mut().unwrap();
+        *x /= 2;
+        c+=1;
+    }
+    println!("{}", h.iter().sum::<u64>());
+}
+/* vim:set foldmethod=marker: */
