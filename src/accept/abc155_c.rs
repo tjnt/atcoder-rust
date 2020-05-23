8d0d9f0a6d1fc7c1fec7824408e97b9f3c98351f
tjnt <3m2dyzxu@gmail.com>
2020-05-23 20:02:40 +0900

    add ac

---
+++ src/accept/abc155_c.rs
@@ -0,0 +1,67 @@
+/* {{{ */
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+/* }}} */
+
+use std::collections::HashMap;
+
+fn main() {
+    input! {
+        n: usize,
+        ss: [String;n]
+    }
+    let mut h = HashMap::new();
+    for s in ss {
+        *h.entry(s).or_insert(0) += 1;
+    }
+    let m = h.values().max().unwrap();
+    let mut v = h.iter().filter(|&(_,v)| {
+        v == m
+    }).map(|t| t.0).collect::<Vec<_>>();
+    v.sort();
+    for k in v {
+        println!("{}", k);
+    }
+}
+/* vim:set foldmethod=marker: */
---
+++ src/accept/abc156_d.rs
@@ -0,0 +1,213 @@
+/* {{{ */
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+/* }}} */
+
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+type Num = usize;
+const MOD: Num = 1_000_000_007;
+#[derive(Clone, Copy)]
+pub struct ModInt<T: Copy + Clone>(T);
+impl ModInt<Num> {
+    pub fn value(self) -> Num {
+        self.0
+    }
+    pub fn pow(self, e: usize) -> ModInt<Num> {
+        let mut result = ModInt(1);
+        let mut cur = self;
+        let mut e = e;
+        while e > 0 {
+            if e & 1 == 1 {
+                result *= cur;
+            }
+            e >>= 1;
+            cur *= cur;
+        }
+        result
+    }
+    pub fn inv(self) -> ModInt<Num> {
+        self.pow(MOD - 2)
+    }
+    pub fn factorial(self) -> ModInt<Num> {
+        let mut cur = ModInt(1);
+        for i in 2..(self.0 + 1) {
+            cur *= i;
+        }
+        cur
+    }
+}
+impl Add<Num> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn add(self, mut rhs: Num) -> ModInt<Num> {
+        if rhs >= MOD {
+            rhs %= MOD
+        }
+        let mut t = rhs + self.0;
+        if t >= MOD {
+            t -= MOD
+        }
+        ModInt(t)
+    }
+}
+impl Add<ModInt<Num>> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn add(self, rhs: ModInt<Num>) -> ModInt<Num> {
+        self + rhs.0
+    }
+}
+impl AddAssign<Num> for ModInt<Num> {
+    fn add_assign(&mut self, other: Num) {
+        *self = *self + other;
+    }
+}
+impl AddAssign<ModInt<Num>> for ModInt<Num> {
+    fn add_assign(&mut self, other: ModInt<Num>) {
+        *self = *self + other;
+    }
+}
+impl Sub<Num> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn sub(self, mut rhs: Num) -> ModInt<Num> {
+        if rhs >= MOD {
+            rhs %= MOD
+        }
+        let value = if self.0 < rhs {
+            self.0 + MOD - rhs
+        } else {
+            self.0 - rhs
+        };
+        ModInt(value)
+    }
+}
+impl Sub<ModInt<Num>> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn sub(self, rhs: ModInt<Num>) -> ModInt<Num> {
+        self - rhs.0
+    }
+}
+impl SubAssign<Num> for ModInt<Num> {
+    fn sub_assign(&mut self, other: Num) {
+        *self = *self - other;
+    }
+}
+impl SubAssign<ModInt<Num>> for ModInt<Num> {
+    fn sub_assign(&mut self, other: ModInt<Num>) {
+        *self = *self - other;
+    }
+}
+impl Mul<Num> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn mul(self, mut rhs: Num) -> ModInt<Num> {
+        if rhs >= MOD {
+            rhs %= MOD
+        }
+        let t = (self.0 * rhs) % MOD;
+        ModInt(t)
+    }
+}
+impl Mul<ModInt<Num>> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn mul(self, rhs: ModInt<Num>) -> ModInt<Num> {
+        self * rhs.0
+    }
+}
+impl MulAssign<Num> for ModInt<Num> {
+    fn mul_assign(&mut self, rhs: Num) {
+        *self = *self * rhs;
+    }
+}
+impl MulAssign<ModInt<Num>> for ModInt<Num> {
+    fn mul_assign(&mut self, rhs: ModInt<Num>) {
+        *self = *self * rhs;
+    }
+}
+// #[allow(clippy::suspicious_arithmetic_impl)]
+impl Div<Num> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn div(self, mut rhs: Num) -> ModInt<Num> {
+        if rhs >= MOD {
+            rhs %= MOD
+        }
+        self * ModInt(rhs).inv()
+    }
+}
+impl Div<ModInt<Num>> for ModInt<Num> {
+    type Output = ModInt<Num>;
+    fn div(self, rhs: ModInt<Num>) -> ModInt<Num> {
+        self / rhs.0
+    }
+}
+impl DivAssign<Num> for ModInt<Num> {
+    fn div_assign(&mut self, rhs: Num) {
+        *self = *self / rhs
+    }
+}
+impl DivAssign<ModInt<Num>> for ModInt<Num> {
+    fn div_assign(&mut self, rhs: ModInt<Num>) {
+        *self = *self / rhs
+    }
+}
+
+pub fn ncr(n: usize, r: usize) -> ModInt<Num> {
+    let mut a = ModInt(1);
+    let mut b = ModInt(1);
+    for i in 0..r {
+        a *= n-i;
+        b *= i+1;
+    }
+    a / b
+}
+
+fn main() {
+    input! {
+        n: usize,
+        a: usize,
+        b: usize
+    }
+    let mut x = ModInt(2).pow(n) - 1;
+    x -= ncr(n,a).value();
+    x -= ncr(n,b).value();
+    println!("{}", x.value());
+}
+/* vim:set foldmethod=marker: */
